c529ea1de508348ddffdf8805677343fe5f7ba7a
fleeB1 <fowler.lee8@gmail.com>
2022-06-16 05:21:28 +0200

    add getAll and getFirst for contracts

--- src/declarations/musall/musall.did.d.ts
+++ src/declarations/musall/musall.did.d.ts
@@ -1,6 +1,16 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export interface Contract {
+  'id' : bigint,
+  'scope_of_work' : ScopeOfWork,
+  'creator' : Principal,
+  'number_of_tokens' : bigint,
+  'price_of_contract' : PriceOfContract,
+  'creator_rating' : bigint,
+  'contract_description' : ContractDescription,
+  'terms_of_ownership' : TermsOfOwnership,
+}
 export type ContractDescription = string;
 export type NumberOfTokens = bigint;
 export type PriceOfContract = bigint;
@@ -15,8 +25,10 @@ export interface UserSubmission {
   'contract_description' : ContractDescription,
   'terms_of_ownership' : TermsOfOwnership,
 }
-export interface anon_class_21_1 {
+export interface anon_class_22_1 {
   'creator_contract_submitted' : ActorMethod<[UserSubmission], Result>,
+  'getAllContracts' : ActorMethod<[], Array<Contract>>,
+  'getFirstContract' : ActorMethod<[], Contract>,
   'whoami' : ActorMethod<[], string>,
 }
-export interface _SERVICE extends anon_class_21_1 {}
+export interface _SERVICE extends anon_class_22_1 {}
--- src/declarations/musall/musall.did.js
+++ src/declarations/musall/musall.did.js
@@ -12,10 +12,22 @@ export const idlFactory = ({ IDL }) => {
     'terms_of_ownership' : TermsOfOwnership,
   });
   const Result = IDL.Variant({ 'ok' : IDL.Text, 'err' : IDL.Text });
-  const anon_class_21_1 = IDL.Service({
+  const Contract = IDL.Record({
+    'id' : IDL.Nat,
+    'scope_of_work' : ScopeOfWork,
+    'creator' : IDL.Principal,
+    'number_of_tokens' : IDL.Nat,
+    'price_of_contract' : PriceOfContract,
+    'creator_rating' : IDL.Nat,
+    'contract_description' : ContractDescription,
+    'terms_of_ownership' : TermsOfOwnership,
+  });
+  const anon_class_22_1 = IDL.Service({
     'creator_contract_submitted' : IDL.Func([UserSubmission], [Result], []),
+    'getAllContracts' : IDL.Func([], [IDL.Vec(Contract)], []),
+    'getFirstContract' : IDL.Func([], [Contract], []),
     'whoami' : IDL.Func([], [IDL.Text], []),
   });
-  return anon_class_21_1;
+  return anon_class_22_1;
 };
 export const init = ({ IDL }) => { return []; };
