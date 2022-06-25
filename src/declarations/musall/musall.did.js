cec3d28d5cc69e3134d76422ab0943210d52f907
fleeB1 <fowler.lee8@gmail.com>
2022-06-25 21:46:30 +0200

    removed errors and integreted TS

--- src/declarations/musall/musall.did.js
+++ src/declarations/musall/musall.did.js
@@ -1,11 +1,13 @@
 export const idlFactory = ({ IDL }) => {
   const Result = IDL.Variant({ 'ok' : IDL.Text, 'err' : IDL.Text });
+  const URL = IDL.Text;
   const ScopeOfWork = IDL.Text;
   const PriceOfContract = IDL.Nat;
   const ContractDescription = IDL.Text;
   const TermsOfOwnership = IDL.Text;
   const Contract = IDL.Record({
     'id' : IDL.Nat,
+    'url' : URL,
     'scope_of_work' : ScopeOfWork,
     'creator' : IDL.Principal,
     'number_of_tokens' : IDL.Nat,
@@ -14,16 +16,17 @@ export const idlFactory = ({ IDL }) => {
     'contract_description' : ContractDescription,
     'terms_of_ownership' : TermsOfOwnership,
   });
-  const anon_class_25_1 = IDL.Service({
+  const anon_class_23_1 = IDL.Service({
     'creator_contract_submitted' : IDL.Func(
-        [IDL.Text, IDL.Text, IDL.Nat, IDL.Text, IDL.Nat],
+        [IDL.Text, IDL.Text, IDL.Nat, IDL.Text, IDL.Nat, IDL.Text],
         [Result],
         [],
       ),
     'get_all_contracts' : IDL.Func([], [IDL.Vec(Contract)], []),
     'get_first_contract' : IDL.Func([], [Contract], []),
+    'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
     'whoami' : IDL.Func([], [IDL.Text], []),
   });
-  return anon_class_25_1;
+  return anon_class_23_1;
 };
 export const init = ({ IDL }) => { return []; };
